993c74eba18f5c1b47ead529bd04e0ac7defadba
isabel-santiagolewis <isabel.santiagolewis@gmail.com>
2024-01-15 21:51:51 -0500

    Created Java BackEnd

---
+++ jacenterprises/src/main/java/com/example/jacenterprises/JacenterprisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacenterprises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacenterprisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacenterprisesApplication.class, args);
+	}
+
+}
---
+++ jacenterprises/src/test/java/com/example/jacenterprises/JacenterprisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacenterprises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacenterprisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
